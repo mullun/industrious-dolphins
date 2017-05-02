7908e2950ad20d5ebf453f0b51601f15633aeb81
matthewglassman <matthewglassman78@gmail.com>
2017-05-02 16:13:43 -0400

    Imported .Css file to Home.js.  Need to include Home.js File in ReactDOM.Render..?

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SignUp from './SignUp'
-import Home from './Home'
+import SignUp from './SignUp';
+import Home from './Home';
 import './index.css';
 
 ReactDOM.render(
