7918de365f302d833443929cb0d776eafe66e1ba
Guruparan <cns_guru@yahoo.com>
2017-05-03 22:17:58 -0400

    added shell files for the various screens

---
+++ src/BorrowTool.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./BorrowTool.css";
+
+class BorrowTool extends Component {
+	render(){
+		return(
+			<div className="BorrowTool">
+				<h2>BorrowTool Page</h2>
+			</div>
+			);
+	}
+}
+
+export default BorrowTool;
---
+++ src/MyTools.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./MyTools.css";
+
+class MyTools extends Component {
+	render(){
+		return(
+			<div className="MyTools">
+				<h2>MyTools Page</h2>
+			</div>
+			);
+	}
+}
+
+export default MyTools;
