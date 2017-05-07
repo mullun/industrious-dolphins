21e15ec9810b156288d4a91faa8bfdc7e96a9a84
matthewglassman <matthewglassmn78@gmail.com>
2017-05-07 09:43:57 -0400

    Adding back MyTools update

--- src/MyTools.js
+++ src/MyTools.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import "./AddTool";
 import "./MyTools.css";
+// import "../utils/helpers.js";
 
 class MyTools extends Component {
 
-	componentDidMount(){
-		return axios.get("/mytools")
-		.then(function(response){
-			console.log(response);
-			
-			console.log(userEmail);
-		});
+	// componentDidMount(){
+	// 	helpers.getMyToolsUser()
+	// 	.then(function(response){
+	// 		var userID = response.
+	// 	})
 
-	}
+	// }
 
 	render(){
 		return(
-		<div className = "container">
 			<div className="MyTools">
-					<h2>MyTools Page</h2>		
+					<h2>MyTools Page</h2>
+						<div className="UserAddress">
+							{this.props.useraddress}
+						</div>
+						<div className="UsersTools">
+
+						</div>
 			</div>
-			<div className = "UserInfo">
-			{this.}
-		</div>
-			);
+		);
 	}
 }
 
 export default MyTools;
-
