42ad8d4cbec128f3ebfbdc323c8787ef641be997
Julie <jccheng@live.unc.edu>
2017-05-06 21:38:53 -0400

    accidentally messed up BorrowTool.js but I reverted it back (I think)

--- src/BorrowTool.js
+++ src/BorrowTool.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import "./BorrowModal.css";
+import "./BorrowTool.css";
+import Available from "./Available";
+import Unavailable from "./Unavailable";
 
-class BorrowModal extends Component {
-  render(){
-    return(
-      <div className="BorrowModal">
-        <h2>BorrowModal Page</h2>
-      </div>
-      );
-  }
+class BorrowTool extends Component {
+	render(){
+		return(
+			<div className="BorrowTool">
+				<h2>BorrowTool Page</h2>
+				<Available />
+				<Unavailable />
+			</div>
+			);
+	}
 }
 
-export default BorrowModal;
+export default BorrowTool;
