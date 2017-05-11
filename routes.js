adc4d7aeb535a6a928870e29388f43108a82732f
matthewglassman <matthewglassmn78@gmail.com>
2017-05-11 16:06:42 -0400

    Making Changes to Server.JS and Routes.JS in order for React to build

--- routes.js
+++ routes.js
@@ -27,7 +27,7 @@ module.exports = function (app) {
 
 
   // Main "/" Route. This will redirect the user to our rendered React application
-  app.get('/', function(req, res) {
+  app.get('/*', function(req, res) {
     //res.sendFile(__dirname + "/public/index.html");
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
